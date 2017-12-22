f5176480e116b582fc043eafa52bac50858b5771
choostanley <stanleychoo0522@hotmail.com>
2017-12-22 16:27:04 +0800

    Not Resolved Cloudinary

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,6 +28,6 @@ class ItemsController < ApplicationController
 
 	private
 	def item_params
-		params.require(:item).permit(:name, :price)
+		params.require(:item).permit(:name, :price, :image)
 	end
 end
